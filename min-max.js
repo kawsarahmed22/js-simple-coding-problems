e130dff0277ede4997acbcf747d33b72ae36ef0a
Kawsar Ahmed <kawsarahmed4148@gmail.com>
2023-03-05 14:37:19 +0600

    min max with if else cleared

--- min-max.js
+++ min-max.js
@@ -10,3 +10,17 @@ function minNumber (n1, n2, n3){
     return minNumber;
 }
 console.log(minNumber(502,2,3322));
+
+const tamim = 100;
+const mushfiq = 99;
+const miraj = 98;
+
+if (tamim > mushfiq && tamim > miraj){
+    console.log('Tamim is best');
+}
+else if (mushfiq > tamim && mushfiq > miraj){
+    console.log('Mushfiq is best');
+}
+else{
+    console.log('Miraj is best');
+}
